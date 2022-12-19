a5ff28d124012281e38a475234d79e7949643a9e
mohammed0x8 <mohammed08774@gmail.com>
2022-12-19 20:23:19 +0000

    vigenere in python

--- weeks/week1/vigenere.py
+++ weeks/week1/vigenere.py
@@ -1,5 +1,3 @@
-# Implementation 1
-
 def vigenere(key, message):
     message = message.lower()
     message = message.replace(' ','')
@@ -7,43 +5,24 @@ def vigenere(key, message):
     cipher_text = ''
     for i in range(len(message)):
         letter = message[i]
-        k= key[i % m] 
+        k= ord(key[i % m])-97
         cipher_text = cipher_text + chr ((ord(letter) - 97 + k ) % 26 + 97)
-
     return cipher_text
 
 
-# Implementation 2
+def vigenere_decrypt(key, ciphertext):
+    message = ''
+    m = len(key) 
+    for i in range(len(ciphertext)):
+        letter = ciphertext[i]
+        k = -1*(ord(key[i % m])-97)
+        message = message + chr ((ord(letter) - 97 + k ) % 26 + 97)
+    return message 
 
-def generateKey(string, key): 
-  key = list(key) 
-  if len(string) == len(key): 
-    return(key) 
-  else: 
-    for i in range(len(string) -len(key)): 
-      key.append(key[i % len(key)]) 
-  return("" . join(key)) 
-  
-def encryption(string, key): 
-  encrypt_text = [] 
-  for i in range(len(string)): 
-    x = (ord(string[i]) +ord(key[i])) % 26
-    x += ord('A') 
-    encrypt_text.append(chr(x)) 
-  return("" . join(encrypt_text)) 
-
-def decryption(encrypt_text, key): 
-  orig_text = [] 
-  for i in range(len(encrypt_text)): 
-    x = (ord(encrypt_text[i]) -ord(key[i]) + 26) % 26
-    x += ord('A') 
-    orig_text.append(chr(x)) 
-  return("" . join(orig_text)) 
 
 if __name__ == "__main__": 
-  string = input("Enter the message: ")
-  keyword = input("Enter the keyword: ")
-  key = generateKey(string, keyword) 
-  encrypt_text = encryption(string,key) 
+  message = input("Enter the message: ")
+  key = input("Enter the keyword: ")
+  encrypt_text = vigenere(key,message) 
   print("Encrypted message:", encrypt_text) 
-  print("Decrypted message:", decryption(encrypt_text, key)) 
+  print("Decrypted message:", vigenere_decrypt(key, encrypt_text)) 
